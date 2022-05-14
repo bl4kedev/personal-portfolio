951809dbe184b68dbbaec0b1a043139ba4d8799b
BlakeETH <blakedeveloper@outlook.com>
2022-05-14 12:11:15 -0600

    SM Breakpoints agregados

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,13 @@ module.exports = {
         "blue-gray": "#45567D"
       },
       gridTemplateRow: {
-        'layout': 'repeat(5, minmax(auto, 1fr))'
+        'layout': 'repeat(5, minmax(auto, 1fr))',
+        'layout-sm': 'repeat(3, minmax(auto, 1fr))',
+        'layout-md': 'repeat(2, minmax(auto, 1fr))'
+      },
+      gridTemplateColumns: {
+        'layout-sm': 'repeat(2, minmax(auto, 1fr))',
+        'layout-md': 'repeat(3, minmax(auto, 1fr))'
       }
     },
   },
